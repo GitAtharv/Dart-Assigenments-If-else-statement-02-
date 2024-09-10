95d6ddfb51866776a296df6ec70f78900ac54514
GitAtharv <163186776+GitAtharv@users.noreply.github.com>
2024-09-10 20:18:16 +0530

    Add files via upload

---
+++ program3.dart
@@ -0,0 +1,15 @@
+void main(){
+
+ int x=45;
+
+if(x>=30 && x<=50){
+
+print("Number in correct range");
+
+}else{
+
+print("Invalid Range");
+
+}
+
+}
---
+++ program7.dart
@@ -0,0 +1,14 @@
+void main(){
+  int noOfPersons=3;
+
+  if(noOfPersons<8){
+
+    print("You can enter the lift");
+
+  }else{
+
+    print("You can't enter the lift");
+
+  }
+
+}
